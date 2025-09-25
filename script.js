9494e5b896c2ce9fa326a4b0adde046fe334a287
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-25 23:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+const allDivs=document.querySelectorAll(".imgDiv");
+const allImgs=document.querySelectorAll(".img");
+allImgs.forEach((img)=>{
+	img.addEventListener("dragstart",(e)=>{
+		let currImgId=e.target.id;
+		let currDivId=`div${currImgId[currImgId.length-1]}`;
+		e.dataTransfer.setData("text",currDivId);
+	})
+});
+allDivs.forEach((div)=>{
+	div.addEventListener("dragover",(e)=>{
+		e.preventDefault();
+	})
+	div.addEventListener("drop",(e)=>{
+		let div=document.getElementById(e.dataTransfer.getData("text"));
+		let draggedImg=div.childNodes[0];
+		console.log(draggedImg);
+		e.target.src=draggedImg.src;
+		draggedImg.src=e.target.src;
+		
+		
+	})
+})
