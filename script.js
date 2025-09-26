5c414455b4ebc4e9b17236118cbca1b892ab0eb4
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-26 07:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const allImgs=document.querySelectorAll(".img");
 allImgs.forEach((img)=>{
 	img.addEventListener("dragstart",(e)=>{
 		let currImgId=e.target.id;
-		let currDivId=`div${currImgId[currImgId.length-1]}`;
-		e.dataTransfer.setData("text",currDivId);
+		// let currDivId=`div${currImgId[currImgId.length-1]}`;
+		// e.dataTransfer.setData("text",currDivId);
+		e.dataTransfer.setData("text",currImgId);
 	})
 });
 allDivs.forEach((div)=>{
@@ -13,11 +14,14 @@ allDivs.forEach((div)=>{
 		e.preventDefault();
 	})
 	div.addEventListener("drop",(e)=>{
-		let div=document.getElementById(e.dataTransfer.getData("text"));
-		let draggedImg=div.childNodes[0];
-		console.log(draggedImg);
+		// let div=document.getElementById(e.dataTransfer.getData("text"));
+		// let draggedImg=div.childNodes[0];
+		let draggedImgId=e.dataTransfer.getData("text");
+		let draggedImg=document.getElementById(draggedImgId);
+		
+		let temp=e.target.src;
 		e.target.src=draggedImg.src;
-		draggedImg.src=e.target.src;
+		draggedImg.src=temp;
 		
 		
 	})
